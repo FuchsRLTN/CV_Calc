0ffc52259fbcd80cb03a8db63722f4861abe9910
FuchsRLTN <fuchs@rltn.de>
2023-11-01 16:59:04 +0100

    Toogl2

--- Calculator.py
+++ Calculator.py
@@ -16,7 +16,7 @@ toggle_clvcalc = st.toggle('Individuell berechnen')
 
 if toggle_clvcalc:
     sales_avg_ncprice = st.slider("Durschnittlicher NW-Preis (in EUR) festlegen", min_value=10000, max_value=30000, step=500)
-    sales_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=10, step=0.5)
+    sales_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=10, step=1)
     sales_clv = sales_avg_ncprice / 100 * sales_avg_margin
 
     service_avg_revenue = st.slider("Durschnittlicher Umsatz L&T (p.A.; in EUR) festlegen", min_value=0, max_value=5000, step=50)
