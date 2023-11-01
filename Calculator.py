f33adadc0bb8d394d3d5c512612d36a5216068aa
FuchsRLTN <fuchs@rltn.de>
2023-11-01 14:08:23 +0100

    Test Colums

--- Calculator.py
+++ Calculator.py
@@ -3,13 +3,38 @@ import streamlit as st
 
 ## App Header
 
-## st.titel("Hello World 2")
 
 
 ## Config Dealer Data
 
 dealer_jvz = st.slider("JVZ eingeben:", min_value=1, max_value=1000, value=300, step=50)
 dealer_bevshare = st.slider("Anteil EV eingeben:", min_value=1, max_value=100, value=30, step=1)
+st.write("")
+st.divider()
+
+
+
+
+## Customer Value
+
+clv_ev = 2500
+clv_ice = 5776
+
+
+st.header("Kundenwert:")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("EV-Kunde")
+    st.metric(clv_ev)
+
+with col2:
+    st.header("ICE-Kunde")
+    st.metric(clv_ice)
+
+
+
 
 
 ## Details CLV Calculation as an expander
@@ -17,4 +42,4 @@ dealer_bevshare = st.slider("Anteil EV eingeben:", min_value=1, max_value=100, v
 with st.expander("Details zur Kundenwertberechnung:"):
     st.header("Rechenweg")
 
-    
+
