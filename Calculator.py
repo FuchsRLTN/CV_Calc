bbf275ee0babb8f36049a0ab80ca0c44fd5001ec
FuchsRLTN <fuchs@rltn.de>
2023-11-01 12:49:36 +0100

    Test des Sliders

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.text("Hello World 2")
+
+## App Header
+
+st.titel("Hello World 2")
+
+
+## Config Dealer Data
+
+dealer_jvz = st.slider("JVZ eingeben:", min_value=1, max_value=1000, value=300, step=50)
+st.write(dealer_jvz)
+
+
+
