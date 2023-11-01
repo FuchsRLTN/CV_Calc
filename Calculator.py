49b30c0e6d6f04a5721cb99b9a17b1907a9d1db1
FuchsRLTN <fuchs@rltn.de>
2023-11-01 20:26:35 +0100

    5656

--- Calculator.py
+++ Calculator.py
@@ -107,7 +107,7 @@ chart_data = pd.DataFrame(data)
 
 st.dataframe(data)
 
-st.bar_chart(data=chart_data, *, x= "Kundenbestand", y= "Wert")
+st.bar_chart(data=chart_data, x= "Kundenbestand", y= "Wert")
 
 
 st.divider()
