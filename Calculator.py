823e06c838ebecffa7b0e73d9b5ea07f6a8b2bab
FuchsRLTN <fuchs@rltn.de>
2023-11-01 18:16:25 +0100

    final for today

--- Calculator.py
+++ Calculator.py
@@ -30,10 +30,12 @@ if toggle_clvcalc is False:
     total_clv = 5776
     
 else:
+    st.write("Bruttoertrag pro Neuwagen berechnen:")
     sales_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=10, step=1)
     sales_avg_ncprice = st.slider("Durschnittlicher NW-Preis (in EUR) festlegen", min_value=10000, max_value=30000, step=500)
+    st.write("")
     sales_clv = sales_avg_ncprice / 100 * sales_avg_margin
-
+    st.write("Bruttoertrag im Service p.a. berechnen:")
     service_avg_revenue = st.slider("Durschnittlicher Umsatz L&T (p.A.; in EUR) festlegen", min_value=0, max_value=5000, step=50)
     service_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=100, step=1)
     service_clv = service_avg_revenue / 100 * service_avg_margin
