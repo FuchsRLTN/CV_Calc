396c5716843485abe87da576085ee6c62ff1d66c
FuchsRLTN <fuchs@rltn.de>
2023-11-01 18:03:14 +0100

    2

--- Calculator.py
+++ Calculator.py
@@ -47,7 +47,8 @@ else:
 
 ## Customer Value Displaying
 
-total_clv_str = str(total_clv)+" EUR pro Kunde"
+currency = " EUR pro Kunde"
+total_clv_str = str(total_clv)+ currency
 st.write(total_clv_str)
 st.info(total_clv_str, icon="i")
 st.caption("Durchschnittlicher Ertrag erwirtschaftet pro Kunde nach 8 Jahren")
