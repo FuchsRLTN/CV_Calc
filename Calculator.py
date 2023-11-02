ceb7f3f5f789b90b77f207c3c34f74d207fc45ab
FuchsRLTN <fuchs@rltn.de>
2023-11-02 08:58:04 +0100

    Text Changes --> Bold

--- Calculator.py
+++ Calculator.py
@@ -18,11 +18,11 @@ st.write("")
 
 
 if toggle_clvcalc is False:
-    st.write("Bruttoertrag pro Neuwagen:")
+    st.write("***Bruttoertrag pro Neuwagen:***")
     st.write("2.080 EUR (26.000 € x 8%)")
     st.write("")
     st.write("")
-    st.write("Bruttoertrag im Service p. a.:")
+    st.write("***Bruttoertrag im Service p. a.:***")
     st.write("462 € (ca. 1.100 € Umsatz Lohn + Teile x 42%)")
 
     sales_clv = 2080
@@ -30,13 +30,13 @@ if toggle_clvcalc is False:
     total_clv = 5776
     
 else:
-    st.write("Bruttoertrag pro Neuwagen berechnen:")
+    st.write("***Bruttoertrag pro Neuwagen berechnen:***")
     sales_avg_ncprice = st.slider("Durschnittlicher NW-Preis (in EUR) festlegen", min_value=10000, max_value=30000, value=26000, step=500)
     sales_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=10, value=8, step=1)
     sales_clv = sales_avg_ncprice / 100 * sales_avg_margin
     st.write("")
     st.write("")
-    st.write("Bruttoertrag im Service p.a. berechnen:")
+    st.write("***Bruttoertrag im Service p.a. berechnen:***")
     service_avg_revenue = st.slider("Durschnittlicher Umsatz L&T (p.A.; in EUR) festlegen", min_value=0, max_value=5000, value=1100, step=50)
     service_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=100, value=42, step=1)
     service_clv = service_avg_revenue / 100 * service_avg_margin
