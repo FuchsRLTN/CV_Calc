88d4effd0403aa17e564a2a6b6216a80e8db3524
FuchsRLTN <fuchs@rltn.de>
2023-11-02 08:39:43 +0100

    Updater with neu subheader

--- Calculator.py
+++ Calculator.py
@@ -9,11 +9,10 @@ st.write("")
 st.write("")
 
 
-## Customer Value Calculation
+## Customer Value: Calculation
 
 
-st.subheader("KUNDENWERT")
-
+st.image("https://i.postimg.cc/26mBKVM1/HMD-H3-Kunden.png")
 
 
 toggle_clvcalc = st.toggle('Individuell berechnen')
@@ -34,14 +33,14 @@ if toggle_clvcalc is False:
     
 else:
     st.write("Bruttoertrag pro Neuwagen berechnen:")
-    sales_avg_ncprice = st.slider("Durschnittlicher NW-Preis (in EUR) festlegen", min_value=10000, max_value=30000, step=500)
-    sales_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=10, step=1)
+    sales_avg_ncprice = st.slider("Durschnittlicher NW-Preis (in EUR) festlegen", min_value=10000, max_value=30000, value=26000, step=500)
+    sales_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=10, value=8, step=1)
     sales_clv = sales_avg_ncprice / 100 * sales_avg_margin
     st.write("")
     st.write("")
     st.write("Bruttoertrag im Service p.a. berechnen:")
-    service_avg_revenue = st.slider("Durschnittlicher Umsatz L&T (p.A.; in EUR) festlegen", min_value=0, max_value=5000, step=50)
-    service_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=100, step=1)
+    service_avg_revenue = st.slider("Durschnittlicher Umsatz L&T (p.A.; in EUR) festlegen", min_value=0, max_value=5000, value=1100, step=50)
+    service_avg_margin = st.slider("Durchschnittliche Marge (%) festlegen", min_value=1, max_value=100, value=42, step=1)
     service_clv = service_avg_revenue / 100 * service_avg_margin
 
     st.write(sales_clv)
@@ -52,7 +51,7 @@ else:
  
 
 
-## Customer Value Displaying
+## Customer Value: Result
 
 st.write("")
 st.write("")
@@ -67,7 +66,12 @@ st.divider()
 st.write("")
 st.write("")
 
-st.subheader("KUNDENBESTAND")
+
+
+
+## Customer Equity: Calculation
+
+st.image("https://i.postimg.cc/kg3McLk4/HMD-H3-Bestand.png")
 st.write("")
 st.write("")
 
@@ -76,7 +80,7 @@ st.write("")
 st.write("")
 
 dealer_jvz = st.slider("JVZ eingeben:", min_value=1, max_value=1000, value=300, step=50)
-## dealer_bevshare = st.slider("Anteil EV eingeben:", min_value=1, max_value=100, value=30, step=1)
+dealer_loyalty_improvement = st.slider("Kundenbindung erhöhen auf:", min_value=6, max_value=10, value=8, step=1)
 st.write("")
 st.divider()
 
