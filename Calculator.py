b4c3e2bcb2c2f262c0c784a493efb9c5666d3932
FuchsRLTN <fuchs@rltn.de>
2024-04-12 13:48:13 +0200

    final patch

--- Calculator.py
+++ Calculator.py
@@ -356,17 +356,11 @@ data_peryear = {
     ## "Bruttoertrag V&S - Gebunden": [bestand_equity_loyal_y1, bestand_equity_loyal_y2, bestand_equity_loyal_y3, bestand_equity_loyal_y4, bestand_equity_loyal_y5, bestand_equity_loyal_y6, bestand_equity_loyal_y7, bestand_equity_loyal_y8, bestand_equity_loyal_y9, bestand_equity_loyal_y10]
 }
 
-## data_year = {["1.Jahr", "2.Jahr", "3.Jahr","4.Jahr", "5.Jahr", "6.Jahr","7.Jahr", "8.Jahr", "9.Jahr","10.Jahr"]}
-
-
-st.write("**Ihr Zusatzertrag nach Jahren**" )
 
 
 
-## Creating the bar chart 
-st.bar_chart(data=data_peryear, x="Jahr", color=["#002B5E", "#00AAD2"])
 
-
+## Creating the bar chart in an table
 
 kundenbestand_bar_col1_empty, kundenbestand_bar_col2_chart = st.columns([0.1, 0.9])
 
@@ -375,6 +369,7 @@ with kundenbestand_bar_col1_empty:
     
 
 with kundenbestand_bar_col2_chart:
+    st.write("**Ihr Zusatzertrag nach Jahren**" )
     st.bar_chart(data=data_peryear, x="Jahr", color=["#002B5E", "#00AAD2"])
 
 
